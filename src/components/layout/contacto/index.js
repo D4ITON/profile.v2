42c886b791eb45b89a50fbe750e1c7980d30ac36
D4ITON <daitonmh100@gmail.com>
2020-02-22 15:59:33 -0500

    contacto tablet version

--- src/components/layout/contacto/index.js
+++ src/components/layout/contacto/index.js
@@ -7,17 +7,18 @@ import icon_whatsapp from '../../../assets/whatsapp.png';
 const Contacto = () => { 
     return (
         <div>
-            <div className="about contacto" id="contacto">
+            <div className="contacto" id="contacto">
                 <h2 className="title-section">Contacto</h2>
-                <div className="panel">
-                    <div className="panel-header">
+                <div className="c-panel">
+                    <div className="c-p-header">
                         <div className="image-circle"><img src={profile_icon} alt="profile icon"/></div>
                         <div className="name-text">
                             <h4>Dalthon Mamani Hualpa</h4>
                             <p>Front-End Developer</p>
                         </div>
+                        <button className="button-primary">Download CV</button>
                     </div>
-                    <div className="redes">
+                    <div className="c-p-redes">
                         <div className="red-item"><img src={icon_whatsapp} alt=""/><p>(51) 930 392 725</p></div>
                         <div className="red-item"><img src={icon_email} alt=""/><p>daitonmh100@gmail.com</p></div>
                         <div className="red-item"><img src={icon_global} alt=""/><p>www.dalthon.ml</p></div>
