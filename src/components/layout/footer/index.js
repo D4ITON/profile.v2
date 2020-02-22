0ef41b1aecb5d49c800929687f99d3c2360ef6c5
D4ITON <daitonmh100@gmail.com>
2020-02-22 16:09:38 -0500

    footer tablet version

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -7,23 +7,25 @@ import logo_github from '../../../assets/github.png';
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="f-name">
-                <img src={logo_dm} alt="Logo DM"/>
-                <h4>Dalthon Mamani Hualpa</h4>
-                <p>Front-End Developer</p>
-            </div>
-            <div className="f-about">
-                <p>Sobre</p>
-                <a href="#">Sobre mí</a>
-                <a href="#">Mis habilidades</a>
-                <a href="#">Portafolio</a>
-            </div>
-            <div className="f-hello">
-                <b>Di hola</b>
-                <div className="f-h-link">
-                    <a href="#"><img src={logo_github} alt="Logo Github"/></a>
-                    <a href="#"><img src={logo_linkedin} alt="Logo Linkedin"/></a>
-                    <a href="#"><img src={logo_twitter} alt="Logo Twitter"/></a>
+            <div className="f-content">
+                <div className="f-name">
+                    <img src={logo_dm} alt="Logo DM"/>
+                    <h4>Dalthon Mamani Hualpa</h4>
+                    <p>Front-End Developer</p>
+                </div>
+                <div className="f-about">
+                    <p>Sobre</p>
+                    <a href="#">Sobre mí</a>
+                    <a href="#">Mis habilidades</a>
+                    <a href="#">Portafolio</a>
+                </div>
+                <div className="f-hello">
+                    <b>Di hola</b>
+                    <div className="f-h-link">
+                        <a href="#"><img src={logo_github} alt="Logo Github"/></a>
+                        <a href="#"><img src={logo_linkedin} alt="Logo Linkedin"/></a>
+                        <a href="#"><img src={logo_twitter} alt="Logo Twitter"/></a>
+                    </div>
                 </div>
             </div>
             <div className="f-cright">
