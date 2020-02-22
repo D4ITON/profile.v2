948f24b0756711574440bc8098e6e2d3134b73f0
D4ITON <daitonmh100@gmail.com>
2020-02-22 10:20:00 -0500

    abilities version tablet

--- src/components/layout/abilities/index.js
+++ src/components/layout/abilities/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import abilities from './abilities';
-import img_react from '../../../assets/logo-react.png';
+import img_react from '../../../assets/react.png';
 
 const Abilities = () => {
     
     return (
         <div className="abilities" id="abilities">
             <h2 className="title-section">Mis habilidades</h2>
-            <div className="abilities-list">
-                {
-                    abilities.map(item => (
-                        <div className="item" key={item.id}>
-                            <div className="img"><img src={item.img} alt="image"/></div>
-                            <p>{item.title}</p>
-                        </div>
-                    ))
-                }
+            <div className="a-container">
+                <div className="a-aside">
+                    <img src={img_react} alt="react image"/>
+                </div>
+                <div className="abilities-list">
+                    <div className="group">
+                        {
+                            abilities.map((item, index) => (
+                                (index < 3) ? (
+                                    <div className="item" key={item.id}>
+                                        <div className="img"><img src={item.img} alt="image"/></div>
+                                        <p>{item.title}</p>
+                                    </div>
+                                ) : false ))
+                        }
+                    </div>
+                    <div className="group">
+                        {
+                            abilities.map((item, index) => (
+                                (index >= 3) ? (
+                                    <div className="item" key={item.id}>
+                                        <div className="img"><img src={item.img} alt="image"/></div>
+                                        <p>{item.title}</p>
+                                    </div>
+                                ) : false ))
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
